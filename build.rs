85536521fa67fa9e14182f9962456a39249565b9
moonie <67006455+Kim-Dewelski@users.noreply.github.com>
2024-05-04 16:26:21 +0200

    prevented needing to rebuild the binding all the time

--- build.rs
+++ build.rs
@@ -3,20 +3,19 @@ use std::path::PathBuf;
 use bindgen::EnumVariation;
 
 fn main() {
-    println!("cargo:rustc-link-lib=binding");
-    let bindings = bindgen::builder()
-        .header("fpops.h")
-        .default_enum_style(EnumVariation::ModuleConsts)
-        .use_core()
-        .ignore_methods()
-        .merge_extern_blocks(true)
-        .generate()
-        .expect("failed to generate rust bindings");
     let binding_path = PathBuf::from("binding/binding.rs");
-    if binding_path.exists() {
-        std::fs::remove_file(&binding_path).unwrap()
+    if !binding_path.exists() {
+        let bindings = bindgen::builder()
+            .header("fpops.h")
+            .default_enum_style(EnumVariation::ModuleConsts)
+            .use_core()
+            .ignore_methods()
+            .merge_extern_blocks(true)
+            .generate()
+            .expect("failed to generate rust bindings");
+        bindings
+            .write_to_file(&binding_path)
+            .expect("failed to write binding to file");
     }
-    bindings
-        .write_to_file(&binding_path)
-        .expect("failed to write binding to file");
+    println!("cargo:rustc-link-lib=binding");
 }
